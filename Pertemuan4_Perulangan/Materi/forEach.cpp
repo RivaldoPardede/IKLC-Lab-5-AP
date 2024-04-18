69436fff8101668057cfb9c65debfbb9544a0426
RivaldoPardede <rivaldopardede3@gmail.com>
2024-04-18 21:17:26 +0700

    Materi Pertemuan-4

---
+++ Pertemuan4_Perulangan/Materi/forEach.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  system("CLS");
+  
+  string nama;
+
+  cout << "Input nama anda: ";
+  cin >> nama;
+
+  for (auto c : nama) {
+    c = tolower(c);
+    if (c == 'a' || c == 'i' || c == 'u' || c == 'e') {
+      c = 'o';
+    }
+    cout << c; 
+  }
+  
+  
+  return 0;
+}
